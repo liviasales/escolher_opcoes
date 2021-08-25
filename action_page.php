3653d8cfbeffc02a158e892a96925c6b858476d3
liviasales <livia.fsb@gmail.com>
2021-08-25 15:06:11 -0300

    changes

--- action_page.php
+++ action_page.php
@@ -1,6 +1,29 @@
 <?php
 
-var_dump($_POST);
+//var_dump($_POST);
 
+$opcao = $_POST['fav_option'];
+$item  = $_POST['fav_item'];
+
+$size = sizeof($_POST['fav_item']);
+//print_r(v);
+//exit;
+if($opcao == '1') {
+    echo "Você escolheu a primeira opção!";
+} else if($opcao == '2') {
+    echo "Você escolheu a segunda opção!";
+} else {
+    echo "Você escolheu a terceira opção!";
+}
+
+    echo "<br> Você selecionou o(s) item(ns): " ;
+foreach ($_POST['fav_item'] as $itens){
+    echo $itens;
+
+    if($size > 1){
+        echo ", ";
+    }
+
+}
 
 ?>
